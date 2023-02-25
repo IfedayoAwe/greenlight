38c61fe1c3424705a13232adcee793d3fdf86c0a
IfedayoAwe <olalekanawe99@gmail.com>
2023-02-25 11:21:34 +0100

    reduce boilerplate code

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -23,29 +23,22 @@ type application struct {
 
 func main() {
 	var cfg config
-
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
 	flag.Parse()
-
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
-
 	app := &application{
 		config: cfg,
 		logger: logger,
 	}
-
-	mux := http.NewServeMux()
-	mux.HandleFunc("/v1/healthcheck", app.healthcheckHandler)
-
+	// Use the httprouter instance returned by app.routes() as the server handler.
 	srv := &http.Server{
 		Addr:         fmt.Sprintf(":%d", cfg.port),
-		Handler:      mux,
+		Handler:      app.routes(),
 		IdleTimeout:  time.Minute,
 		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 30 * time.Second,
 	}
-
 	logger.Printf("starting %s server on %s", cfg.env, srv.Addr)
 	err := srv.ListenAndServe()
 	logger.Fatal(err)
