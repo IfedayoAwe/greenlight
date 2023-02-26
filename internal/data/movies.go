54f7542211bc99e3c8110b6b5b5532ff36faaab8
IfedayoAwe <olalekanawe99@gmail.com>
2023-02-26 20:25:35 +0100

    Using error responses

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -5,8 +5,8 @@ import (
 )
 
 type Movie struct {
-	ID        int64     `json:"-"`
-	CreatedAt time.Time `json:"created_at"`
+	ID        int64     `json:"id"`
+	CreatedAt time.Time `json:"-"`
 	Title     string    `json:"title"`
 	Year      int32     `json:"year,omitempty"`
 	Runtime   Runtime   `json:"runtime,omitempty"`
