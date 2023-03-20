84654d06a9a4e21b99fa3ced8fecb5788fe92894
IfedayoAwe <olalekanawe99@gmail.com>
2023-03-20 19:49:22 +0100

    custom metrics

--- cmd/examples/cors/simple/main.go
+++ cmd/examples/cors/simple/main.go
@@ -6,18 +6,53 @@ import (
 	"net/http"
 )
 
-const html = `
+// const html = `
+// <!DOCTYPE html>
+// <html lang="en">
+// <head>
+// 	<meta charset="UTF-8">
+// </head>
+// <body>
+// 	<h1>Simple CORS</h1>
+// 	<div id="output"></div>
+// 	<script>
+// 		document.addEventListener('DOMContentLoaded', function() {
+// 			fetch("http://localhost:4000/v1/healthcheck").then(
+// 				function (response) {
+// 					response.text().then(function (text) {
+// 						document.getElementById("output").innerHTML = text;
+// 					});
+// 				},
+// 				function(err) {
+// 					document.getElementById("output").innerHTML = err;
+// 				}
+// 			);
+// 		});
+// 	</script>
+// </body>
+// </html>`
+
+const html1 = `
 <!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
 </head>
 <body>
-	<h1>Simple CORS</h1>
+	<h1>Preflight CORS</h1>
 	<div id="output"></div>
 	<script>
 		document.addEventListener('DOMContentLoaded', function() {
-			fetch("http://localhost:4000/v1/healthcheck").then(
+			fetch("http://localhost:4000/v1/tokens/authentication", {
+				method: "POST",
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify({
+					email: 'oaleknaw99@gmail.com',
+					password: '1234567890'
+				})
+			}).then(
 				function (response) {
 					response.text().then(function (text) {
 						document.getElementById("output").innerHTML = text;
@@ -33,14 +68,11 @@ const html = `
 </html>`
 
 func main() {
-	// Make the server address configurable at runtime via a command-line flag.
 	addr := flag.String("addr", ":9000", "Server address")
 	flag.Parse()
 	log.Printf("starting server on %s", *addr)
-	// Start a HTTP server listening on the given address, which responds to all
-	// requests with the webpage HTML above.
 	err := http.ListenAndServe(*addr, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(html))
+		w.Write([]byte(html1))
 	}))
 	log.Fatal(err)
 }
