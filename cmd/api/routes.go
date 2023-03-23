bab3b57c423789c2712dec9859354635735f416c
IfedayoAwe <olalekanawe99@gmail.com>
2023-03-23 01:07:07 +0100

    reset password

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -21,6 +21,7 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodPut, "/v1/users/activated", app.activateUserHandler)
 	router.HandlerFunc(http.MethodPut, "/v1/users/password", app.resetUserPasswordHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/tokens/authentication", app.createAuthenticationTokenHandler)
+	router.HandlerFunc(http.MethodPost, "/v1/tokens/activation", app.createActivationTokenHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/tokens/password-reset", app.createPasswordResetTokenHandler)
 	router.HandlerFunc(http.MethodPut, "/v1/users/change-password", app.requireActivatedUser(app.changePasswordHandler))
 	router.Handler(http.MethodGet, "/debug/vars", expvar.Handler())
