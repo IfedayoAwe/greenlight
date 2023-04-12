49cd340bb42f50dd3a6ae396120c389294bbf6a8
IfedayoAwe <olalekanawe99@gmail.com>
2023-04-12 15:32:49 +0100

    TestCreateActivationToken

--- cmd/api/tokens_test.go
+++ cmd/api/tokens_test.go
@@ -64,3 +64,56 @@ func TestCreateAuthenticationToken(t *testing.T) {
 		})
 	}
 }
+
+func TestCreateActivationToken(t *testing.T) {
+	app := newTestApplication(t)
+	ts := newTestServer(t, app.routes())
+	defer ts.Close()
+
+	tests := []struct {
+		name     string
+		email    string
+		wantCode int
+		wantBody []byte
+	}{
+		{"Created", "ayo@gmail.com", http.StatusAccepted, []byte("an email will be sent to you containing activation instructions")},
+		{"AlreadyActivated", "olalekanawe99@gmail.com", http.StatusUnprocessableEntity, []byte("user has already been activated")},
+		{"InvalidEmail", "gmail.com", http.StatusUnprocessableEntity, []byte("must be a valid email address")},
+		{"UnknownEmail", "foo@gmail.com", http.StatusUnprocessableEntity, []byte("no matching email address found")},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			credentials := struct {
+				Email string
+			}{
+				Email: tt.email,
+			}
+
+			payload, err := json.Marshal(credentials)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			req, err := http.NewRequest(http.MethodPost, ts.URL+"/v1/tokens/activation", bytes.NewReader(payload))
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			req.Header.Add("Content-Type", "application/json")
+
+			code, header, body := ts.do(t, req)
+			if contentType := header.Get("Content-Type"); contentType != "application/json" {
+				t.Errorf("want %q; got %q", "application/json", contentType)
+			}
+
+			if code != tt.wantCode {
+				t.Errorf("want %d; got %d", tt.wantCode, code)
+			}
+
+			if !bytes.Contains(body, tt.wantBody) {
+				t.Errorf("want body to contain %q", tt.wantBody)
+			}
+		})
+	}
+}
